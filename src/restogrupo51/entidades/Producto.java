90e704b414c7ff98c8d6425fab365d95e71fee0d
Alb3rtoGitHub <ravallecillo@hotmail.com>
2023-10-04 20:56:10 -0300

    Clase Producto

--- src/restogrupo51/entidades/Producto.java
+++ src/restogrupo51/entidades/Producto.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package restogrupo51.entidades;
 
-/**
- *
- * @author tobal
- */
 public class Producto {
-    
+    private int idProducto;
+    private String nombreProducto;
+    private double precio;
+    private int stock;
+
+    public Producto() {
+    }
+
+    public Producto(String nombreProducto, double precio, int stock) {
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public Producto(int idProducto, String nombreProducto, double precio, int stock) {
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + idProducto + ", " + nombreProducto + ", " + precio + ", " + stock;
+    }
 }
