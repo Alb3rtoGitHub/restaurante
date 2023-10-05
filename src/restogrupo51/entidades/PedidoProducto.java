0b8de1bf3db49ad217921ce6c24c7a49a73184fa
Nico96C <nicolas.cuello96@hotmail.com>
2023-10-05 10:10:52 -0300

    Clase PedidoProducto

--- src/restogrupo51/entidades/PedidoProducto.java
+++ src/restogrupo51/entidades/PedidoProducto.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package restogrupo51.entidades;
 
-/**
- *
- * @author tobal
- */
 public class PedidoProducto {
+    private int idPedidoProducto;
+    private int idPedido;
+    private int idProducto;
+    private int cantidad;
+
+    public PedidoProducto() {
+    }
+
+    public PedidoProducto(int idPedido, int idProducto, int cantidad) {
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+    }
+
+    public PedidoProducto(int idPedidoProducto, int idPedido, int idProducto, int cantidad) {
+        this.idPedidoProducto = idPedidoProducto;
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdPedidoProducto() {
+        return idPedidoProducto;
+    }
+
+    public void setIdPedidoProducto(int idPedidoProducto) {
+        this.idPedidoProducto = idPedidoProducto;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + idPedidoProducto + ", ID Pedido: " + idPedido + ", ID Producto: " + idProducto + ", Cantidad: " + cantidad;
+    }
+    
     
 }
