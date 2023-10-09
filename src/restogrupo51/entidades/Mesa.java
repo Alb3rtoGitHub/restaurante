3688b7bc5a768c5fe2c628024cd5a94af95e5a4e
Nico96C <nicolas.cuello96@hotmail.com>
2023-10-09 16:34:22 -0300

    Cambios a Clase Mesa

--- src/restogrupo51/entidades/Mesa.java
+++ src/restogrupo51/entidades/Mesa.java
@@ -5,64 +5,72 @@ package restogrupo51.entidades;
 public class Mesa {
     
     private int idMesa;
-    private int numero;
-    private boolean estadoMesa;
+    private int numeroMesa;
     private int capacidad;
-
-    
-    
-    public Mesa(int idMesa, int numero, boolean estadoMesa, int capacidad) {
-        this.idMesa = idMesa;
-        this.numero = numero;
-        this.estadoMesa = estadoMesa;
-        this.capacidad = capacidad;
-    }
-
-    public Mesa(int numero, boolean estadoMesa, int capacidad) {
-        this.numero = numero;
-        this.estadoMesa = estadoMesa;
-        this.capacidad = capacidad;
-    }
+    private boolean disponibilidad;
+    private boolean estadoMesa;
 
     public Mesa() {
     }
 
-    
-    
+    public Mesa(int numeroMesa, int capacidad, boolean disponibilidad, boolean estadoMesa) {
+        this.numeroMesa = numeroMesa;
+        this.capacidad = capacidad;
+        this.disponibilidad = disponibilidad;
+        this.estadoMesa = estadoMesa;
+    }
+
+    public Mesa(int idMesa, int numeroMesa, int capacidad, boolean disponibilidad, boolean estadoMesa) {
+        this.idMesa = idMesa;
+        this.numeroMesa = numeroMesa;
+        this.capacidad = capacidad;
+        this.disponibilidad = disponibilidad;
+        this.estadoMesa = estadoMesa;
+    }
+
     public int getIdMesa() {
         return idMesa;
     }
+
     public void setIdMesa(int idMesa) {
         this.idMesa = idMesa;
     }
 
-    public int getNumero() {
-        return numero;
-    }
-    public void setNumero(int numero) {
-        this.numero = numero;
+    public int getNumeroMesa() {
+        return numeroMesa;
     }
 
-    public boolean isEstadoMesa() {
-        return estadoMesa;
-    }
-    public void setEstadoMesa(boolean estadoMesa) {
-        this.estadoMesa = estadoMesa;
+    public void setNumeroMesa(int numeroMesa) {
+        this.numeroMesa = numeroMesa;
     }
 
     public int getCapacidad() {
         return capacidad;
     }
+
     public void setCapacidad(int capacidad) {
         this.capacidad = capacidad;
     }
 
-    
-    
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public boolean isEstadoMesa() {
+        return estadoMesa;
+    }
+
+    public void setEstadoMesa(boolean estadoMesa) {
+        this.estadoMesa = estadoMesa;
+    }
+
     @Override
     public String toString() {
-        return "Mesa{" + "idMesa=" + idMesa + ", numero=" + numero + ", estadoMesa=" + estadoMesa + ", capacidad=" + capacidad + '}';
+        return "Mesa{" + "idMesa=" + idMesa + ", numeroMesa=" + numeroMesa + ", capacidad=" + capacidad + ", disponibilidad=" + disponibilidad + ", estadoMesa=" + estadoMesa + '}';
     }
-    
     
 }
